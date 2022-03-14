80f242b0bb1028af821ecc3e325c78981a96bedf
cunlu1 <cengizemreunlu99@gmail.com>
2022-03-14 22:07:48 +0300

    Sosyal medya butonları oluşturuldu. Hesaplar eklendi. Görsel düzenlemeler yapıldı. Tarifler için sayfalar açıldı.

---
+++ TripProjectWebApp/Views/Appetizer/AppetizerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TripProjectWebApp.Views.Appetizer
+{
+    public class AppetizerController : Controller
+    {
+        // GET: Appetizer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TripProjectWebApp/Views/MainCourse/MainCourseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TripProjectWebApp.Views.MainCourse
+{
+    public class MainCourseController : Controller
+    {
+        // GET: MainCourse
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
