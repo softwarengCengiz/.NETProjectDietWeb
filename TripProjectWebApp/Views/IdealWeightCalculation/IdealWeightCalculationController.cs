2670e3aff7837861ec6a43785c034dfbab97a5db
Cengiz Emre Ünlü <cengiz.unlu@ogr.sakarya.edu.tr>
2022-03-19 02:22:26 +0300

    İdeal kilo hesabı yapıldı

---
+++ TripProjectWebApp/Views/IdealWeightCalculation/IdealWeightCalculationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TripProjectWebApp.Views.IdealWeightCalculation
+{
+    public class IdealWeightCalculationController : Controller
+    {
+        // GET: IdealWeightCalculation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
